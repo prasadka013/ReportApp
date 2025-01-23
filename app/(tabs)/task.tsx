226524d313798b6b81cf91a020b239bd3ea42e74
Jash <jashgohil786@gmail.com>
2025-01-23 23:47:11 +0530

    changes in task

--- app/(tabs)/task.tsx
+++ app/(tabs)/task.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Task Screen</Text>
+      <TextInput style={styles.input} placeholder="Task Name" placeholderTextColor="#888" />
+      <TextInput style={styles.input} placeholder="Task Description" placeholderTextColor="#888" />
+      <TextInput style={styles.input} placeholder="Task Status" placeholderTextColor="#888" />
     </View>
   );
 }
@@ -19,5 +22,12 @@ const styles = StyleSheet.create({
     text: {
       fontSize: 20,
       color: '#FFF', // Set the text color to white
+    },
+    input: {
+      width: '80%',
+      padding: 10,
+      margin: 10,
+      backgroundColor: '#FFF',
+      borderRadius: 5,
     },
   });
